8f146a26dce0c97e38c438f108e2263f2a0a219b
Tony Hillerson <tony.hillerson@gmail.com>
2011-03-21 11:12:21 -0600

    using correct discussion instead of user discussion id to load responses for a response

--- src/com/ecollege/api/services/discussions/FetchDiscussionResponsesForResponse.java
+++ src/com/ecollege/api/services/discussions/FetchDiscussionResponsesForResponse.java
@@ -3,6 +3,7 @@ package com.ecollege.api.services.discussions;
 import java.util.List;
 import java.util.logging.Logger;
 
+import com.ecollege.api.model.DiscussionResponse;
 import com.ecollege.api.model.UserDiscussionResponse;
 import com.ecollege.api.services.BaseService;
 
@@ -11,6 +12,10 @@ public class FetchDiscussionResponsesForResponse extends BaseService {
 	private static Logger l = Logger.getLogger(FetchDiscussionTopicsForCourseIds.class.getName());
 	private UserDiscussionResponse response;
 	private List<UserDiscussionResponse> result;
+	
+	public List<UserDiscussionResponse> getResult() {
+		return result;
+	}
 
 	public FetchDiscussionResponsesForResponse(UserDiscussionResponse response) {
 		super();
@@ -19,7 +24,8 @@ public class FetchDiscussionResponsesForResponse extends BaseService {
 
 	@Override
 	public String getResource() {
-		return "/me/responses/" + response.getId() + "/userresponses";
+		DiscussionResponse dr = response.getResponse();
+		return "/me/responses/" + dr.getId() + "/userresponses";
 	}
 	
 	@Override
