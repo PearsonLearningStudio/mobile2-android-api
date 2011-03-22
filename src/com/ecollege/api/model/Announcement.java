0322b8e3f74935e04ba4604eae9ff0801e534c88
Tony Hillerson <tony.hillerson@gmail.com>
2011-03-22 15:11:54 -0600

    Adding an announcement DTO

---
+++ src/com/ecollege/api/model/Announcement.java
@@ -0,0 +1,51 @@
+package com.ecollege.api.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+@SuppressWarnings("serial")
+public class Announcement implements Serializable {
+
+	private long id;
+	private String subject;
+	private String text;
+	private String submitter;
+	private Calendar startDisplayDate;
+	private Calendar endDisplayDate;
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setSubmitter(String submitter) {
+		this.submitter = submitter;
+	}
+	public String getSubmitter() {
+		return submitter;
+	}
+	public void setStartDisplayDate(Calendar startDisplayDate) {
+		this.startDisplayDate = startDisplayDate;
+	}
+	public Calendar getStartDisplayDate() {
+		return startDisplayDate;
+	}
+	public void setEndDisplayDate(Calendar endDisplayDate) {
+		this.endDisplayDate = endDisplayDate;
+	}
+	public Calendar getEndDisplayDate() {
+		return endDisplayDate;
+	}
+}
